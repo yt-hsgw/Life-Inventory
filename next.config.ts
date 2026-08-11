a9c88d7e52b68fd66b87de07b50c4b97c826d7c8
yt_hsgw <ythsgw34@gmail.com>
2026-08-11 23:46:50 +0900

    chore: hide Next.js dev indicator

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   allowedDevOrigins: ["127.0.0.1", "192.168.1.142"],
+  devIndicators: false,
   poweredByHeader: false,
   typedRoutes: true,
 };
